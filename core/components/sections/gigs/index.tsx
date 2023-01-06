5e5dc07705d46a6d22397d9a99a065564166b007
ntabucejo <ntabucejo@gmail.com>
2023-01-06 22:20:00 +0800

    chore(html): update section

--- core/components/sections/gigs/index.tsx
+++ core/components/sections/gigs/index.tsx
@@ -2,79 +2,81 @@ import Gig from "./gig";
 
 const Gigs = () => {
   return (
-    <section className="contain grid grid-cols-[repeat(4,auto)] gap-4">
-      <Gig
-        thumbnail="https://images.unsplash.com/photo-1552308995-2baac1ad5490?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-        avatar="https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80"
-        name="Lara Morena"
-        location="Nort America"
-        title="Lorem ipsum dolor sit amet, consectetur adipisicing."
-        category="Web Development"
-        price="$50 - $100"
-      />
-      <Gig
-        thumbnail="https://images.unsplash.com/photo-1552308995-2baac1ad5490?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-        avatar="https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80"
-        name="Lara Morena"
-        location="Nort America"
-        title="Lorem ipsum dolor sit amet, consectetur adipisicing."
-        category="Web Development"
-        price="$50 - $100"
-      />
-      <Gig
-        thumbnail="https://images.unsplash.com/photo-1552308995-2baac1ad5490?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-        avatar="https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80"
-        name="Lara Morena"
-        location="Nort America"
-        title="Lorem ipsum dolor sit amet, consectetur adipisicing."
-        category="Web Development"
-        price="$50 - $100"
-      />
-      <Gig
-        thumbnail="https://images.unsplash.com/photo-1552308995-2baac1ad5490?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-        avatar="https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80"
-        name="Lara Morena"
-        location="Nort America"
-        title="Lorem ipsum dolor sit amet, consectetur adipisicing."
-        category="Web Development"
-        price="$50 - $100"
-      />
-      <Gig
-        thumbnail="https://images.unsplash.com/photo-1552308995-2baac1ad5490?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-        avatar="https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80"
-        name="Lara Morena"
-        location="Nort America"
-        title="Lorem ipsum dolor sit amet, consectetur adipisicing."
-        category="Web Development"
-        price="$50 - $100"
-      />
-      <Gig
-        thumbnail="https://images.unsplash.com/photo-1552308995-2baac1ad5490?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-        avatar="https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80"
-        name="Lara Morena"
-        location="Nort America"
-        title="Lorem ipsum dolor sit amet, consectetur adipisicing."
-        category="Web Development"
-        price="$50 - $100"
-      />
-      <Gig
-        thumbnail="https://images.unsplash.com/photo-1552308995-2baac1ad5490?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-        avatar="https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80"
-        name="Lara Morena"
-        location="Nort America"
-        title="Lorem ipsum dolor sit amet, consectetur adipisicing."
-        category="Web Development"
-        price="$50 - $100"
-      />
-      <Gig
-        thumbnail="https://images.unsplash.com/photo-1552308995-2baac1ad5490?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-        avatar="https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80"
-        name="Lara Morena"
-        location="Nort America"
-        title="Lorem ipsum dolor sit amet, consectetur adipisicing."
-        category="Web Development"
-        price="$50 - $100"
-      />
+    <section className="contain">
+      <ul className="grid grid-cols-[repeat(4,auto)] gap-4">
+        <Gig
+          thumbnail="https://images.unsplash.com/photo-1552308995-2baac1ad5490?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+          avatar="https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80"
+          name="Lara Morena"
+          location="Nort America"
+          title="Lorem ipsum dolor sit amet, consectetur adipisicing."
+          category="Web Development"
+          price="$50 - $100"
+        />
+        <Gig
+          thumbnail="https://images.unsplash.com/photo-1552308995-2baac1ad5490?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+          avatar="https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80"
+          name="Lara Morena"
+          location="Nort America"
+          title="Lorem ipsum dolor sit amet, consectetur adipisicing."
+          category="Web Development"
+          price="$50 - $100"
+        />
+        <Gig
+          thumbnail="https://images.unsplash.com/photo-1552308995-2baac1ad5490?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+          avatar="https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80"
+          name="Lara Morena"
+          location="Nort America"
+          title="Lorem ipsum dolor sit amet, consectetur adipisicing."
+          category="Web Development"
+          price="$50 - $100"
+        />
+        <Gig
+          thumbnail="https://images.unsplash.com/photo-1552308995-2baac1ad5490?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+          avatar="https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80"
+          name="Lara Morena"
+          location="Nort America"
+          title="Lorem ipsum dolor sit amet, consectetur adipisicing."
+          category="Web Development"
+          price="$50 - $100"
+        />
+        <Gig
+          thumbnail="https://images.unsplash.com/photo-1552308995-2baac1ad5490?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+          avatar="https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80"
+          name="Lara Morena"
+          location="Nort America"
+          title="Lorem ipsum dolor sit amet, consectetur adipisicing."
+          category="Web Development"
+          price="$50 - $100"
+        />
+        <Gig
+          thumbnail="https://images.unsplash.com/photo-1552308995-2baac1ad5490?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+          avatar="https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80"
+          name="Lara Morena"
+          location="Nort America"
+          title="Lorem ipsum dolor sit amet, consectetur adipisicing."
+          category="Web Development"
+          price="$50 - $100"
+        />
+        <Gig
+          thumbnail="https://images.unsplash.com/photo-1552308995-2baac1ad5490?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+          avatar="https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80"
+          name="Lara Morena"
+          location="Nort America"
+          title="Lorem ipsum dolor sit amet, consectetur adipisicing."
+          category="Web Development"
+          price="$50 - $100"
+        />
+        <Gig
+          thumbnail="https://images.unsplash.com/photo-1552308995-2baac1ad5490?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+          avatar="https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80"
+          name="Lara Morena"
+          location="Nort America"
+          title="Lorem ipsum dolor sit amet, consectetur adipisicing."
+          category="Web Development"
+          price="$50 - $100"
+        />
+      </ul>
     </section>
   );
 };
