a33234d594d97664c1a9b24bff4f80ad02641423
ntabucejo <ntabucejo@gmail.com>
2023-01-06 22:01:17 +0800

    chore(symbol): update symbol props

--- core/components/elements/symbol/index.tsx
+++ core/components/elements/symbol/index.tsx
@@ -1,20 +1,50 @@
 type Props = {
   Icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
   isHoverDisabled?: boolean;
+  size?: "small" | "medium" | "large";
   className?: string;
 };
 
-const Symbol = ({ Icon, isHoverDisabled, className }: Props) => {
+const Symbol = ({
+  Icon,
+  isHoverDisabled,
+  size = "medium",
+  className,
+}: Props) => {
+  let style = "";
+
+  switch (size) {
+    case "small": {
+      const className = "h-4 w-4";
+      style = className;
+      break;
+    }
+    case "medium": {
+      const className = "h-6 w-6";
+      style = className;
+      break;
+    }
+    case "large": {
+      const className = "h-8 w-8";
+      style = className;
+      break;
+    }
+    default: {
+      style = "no-style";
+    }
+  }
+
   return (
     <Icon
       className={`
-      ${className ? className : ""} 
       ${
         !isHoverDisabled
           ? "hover:cursor-pointer hover:text-primary-dark group-hover:cursor-pointer group-hover:text-primary-dark"
           : ""
       } 
-      icon smooth stroke-2 text-primary-dark/fade`}
+      ${className ? className : ""} 
+      ${style} 
+      smooth aspect-square flex-none stroke-2 text-primary-dark/fade`}
     />
   );
 };
