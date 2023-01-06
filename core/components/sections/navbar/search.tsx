8c4ae324a7a236c56af1af09584ad3400fdb2c4b
ntabucejo <ntabucejo@gmail.com>
2023-01-06 18:11:24 +0800

    chore(search): add ref

--- core/components/sections/navbar/search.tsx
+++ core/components/sections/navbar/search.tsx
@@ -1,11 +1,23 @@
+"use client";
+
 import Symbol from "@core/components/elements/symbol";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+import { useRef, useState } from "react";
 
 const Search = () => {
+  const ref = useRef<any>(null);
+
+  const handleSetFocus = () => {
+    ref.current!.focus();
+  };
+
   return (
-    <div className="clearance flex w-full items-center gap-2 overflow-hidden rounded border bg-white">
+    <div
+      onClick={handleSetFocus}
+      className={`clearance flex w-full items-center gap-2 overflow-hidden rounded border bg-white`}>
       <Symbol Icon={MagnifyingGlassIcon} isHoverDisabled />
       <input
+        ref={ref}
         type="text"
         placeholder="Search here..."
         className="w-full bg-transparent focus:outline-none"
