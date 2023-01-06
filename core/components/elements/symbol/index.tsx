9af42ac29e1a0ffa6bfb7269c087628450ba71ed
ntabucejo <ntabucejo@gmail.com>
2023-01-06 18:05:04 +0800

    chore(symbol): add symbol component for icon

---
+++ core/components/elements/symbol/index.tsx
@@ -0,0 +1,22 @@
+type Props = {
+  Icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+  isHoverDisabled?: boolean;
+  className?: string;
+};
+
+const Symbol = ({ Icon, isHoverDisabled, className }: Props) => {
+  return (
+    <Icon
+      className={`
+      ${className ? className : ""} 
+      ${
+        isHoverDisabled
+          ? ""
+          : "hover:cursor-pointer hover:text-primary-dark group-hover:cursor-pointer group-hover:text-primary-dark"
+      } 
+      icon smooth text-primary-dark/fade`}
+    />
+  );
+};
+
+export default Symbol;
