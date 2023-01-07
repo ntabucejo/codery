f3627db628ce16354d1ff432cca8c7cbb3fb5779
jazztine <jzztn.crz@gmail.com>
2023-01-07 21:10:37 +0800

    chore(gig) : Add gig's third navigation bar

--- app/[username]/gigs/[id]/page.tsx
+++ app/[username]/gigs/[id]/page.tsx
@@ -1,5 +1,19 @@
-const Page = () => {
-  return <div>Page</div>;
+import Tabs from "@core/components/sections/tabs";
+import { Tab } from "@core/types/tab";
+
+const tabLists: Tab[] = [
+  { title: "Overview", isActive: true, href: "#" },
+  { title: "Description", isActive: false, href: "#" },
+  { title: "About the Seller", isActive: false, href: "#" },
+  { title: "Reviews", isActive: false, href: "#" },
+];
+
+const Gig = () => {
+  return (
+    <div>
+      <Tabs tabLists={tabLists} />
+    </div>
+  );
 };
 
-export default Page;
+export default Gig;
