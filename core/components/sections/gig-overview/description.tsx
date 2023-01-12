52b2ab07494051099fcbf22a2c972303bdf566ec
jazztine <jzztn.crz@gmail.com>
2023-01-12 22:15:25 +0800

    chore(gig-overview): create description

---
+++ core/components/sections/gig-overview/description.tsx
@@ -0,0 +1,41 @@
+const Description = () => {
+  return (
+    <div className="mt-20 grid gap-6">
+      <h1 className="text-lg font-bold">About this Gig</h1>
+      <p className="leading-relaxed">
+        Hey, I'm glad to have you here.
+        <br></br>
+        <br></br>
+        I'm Innocent, a modern full stack web developer that has years of
+        expertise working with React Js, Next Js, Tailwind CSS, and Typescript
+        to create amazing projects with clean code, improved user experience,
+        improved performance, fast, flexible, and completely responsive web
+        applications. In today's world, everyone needs fast, responsive, and
+        SEO-optimized websites. Thanks to NextJs and Tailwind CSS, which both
+        together fulfill those requirements. Next.js gives you the best
+        experience of fast, optimized, and production-ready website/web
+        app/SPA/PWA with all the features you needed like : Static Site
+        Generation (SSG) Server-Side Rendering (SSR) Image optimization Inbuild
+        API integration
+        <br></br>
+        <br></br>
+        Working with people like you makes my day. Place your purchase, and I'll
+        build your ideal website.
+        <br></br>
+        <br></br>
+        Why me? 100% client satisfaction Fast and reliable Quality work, and
+        neat and clean code with proper commenting Always use best practices
+        while writing code 24hr availability Great communication
+        <br></br>
+        <br></br>
+        My Services: ReactJs/NextJs website, Figma PSD Adobe XD to
+        ReactJs/NextJs API integration Admin panel Backend
+        <br></br>
+        <br></br>
+        NOTE: Kindly contact me before placing your order.
+      </p>
+    </div>
+  );
+};
+
+export default Description;
