b81db2b343bb0011ca66e0d675f3407b0b94e6e8
ntabucejo <ntabucejo@gmail.com>
2023-01-13 13:28:40 +0800

    chore(avatar): add size props

--- core/components/elements/avatar/index.tsx
+++ core/components/elements/avatar/index.tsx
@@ -3,11 +3,19 @@ import Image from "next/image";
 type Props = {
   src: string;
   alt: string;
+  size: "small" | "medium" | "large";
+  className?: string;
 };
 
-const Avatar = ({ src, alt }: Props) => {
+const Avatar = ({ src, alt, size, className }: Props) => {
   return (
-    <div className="relative h-8 w-8 overflow-hidden">
+    <div
+      className={`
+      ${size === "small" ? "h-8 w-8" : ""} 
+      ${size === "medium" ? "h-12 w-12" : ""} 
+      ${size === "large" ? "h-40 w-40" : ""} 
+      ${className ? className : ""} 
+      relative overflow-hidden`}>
       <Image src={src} alt={alt} fill className="rounded-full object-cover" />
     </div>
   );
