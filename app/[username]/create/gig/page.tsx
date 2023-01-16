5674977d70284c4b958d6be7ba540a1a741d2ca5
ntabucejo <ntabucejo@gmail.com>
2023-01-16 15:00:36 +0800

    feature(create-gig): add create gig page

---
+++ app/[username]/create/gig/page.tsx
@@ -0,0 +1,13 @@
+const Page = () => {
+  return (
+    <>
+      <section className="contain">
+        <div>
+          <input type="text" />
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Page;
