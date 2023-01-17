7bfac95eab16263f31ed8a1a0ef6a280c767b8a4
jazztine <jzztn.crz@gmail.com>
2023-01-17 15:15:36 +0800

    chore(gig-overview): fix heading style

--- core/components/sections/gig/heading.tsx
+++ core/components/sections/gig/heading.tsx
@@ -9,13 +9,13 @@ const Heading = ({ children, number }: Props) => {
   if (number === 1)
     return (
       <h1 className="text-4xl font-bold">
-        <Balancer ratio={3}>
+        <Balancer ratio={2}>
           I will develop fix deploy responsive website with react nextjs
           tailwind
         </Balancer>
       </h1>
     );
-  return <h2 className="mt-24 text-2xl font-bold">{children}</h2>;
+  return <h2 className="mt-12 text-2xl font-bold">{children}</h2>;
 };
 
 export default Heading;
