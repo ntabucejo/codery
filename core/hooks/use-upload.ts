b5650716055ca59cda02107e6c0b4b3924a8b211
ntabucejo <ntabucejo@gmail.com>
2023-01-18 15:16:48 +0800

    chore(hook): add useUpload hook to upload file on cloudinary

---
+++ core/hooks/use-upload.ts
@@ -0,0 +1,44 @@
+import { type FormEvent } from "react";
+
+type UseUpload = {
+  setData: any;
+  inputName: string;
+};
+
+const useUpload = ({ setData, inputName }: UseUpload) => {
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const fileInput = Array.from(form.elements).find(
+      // @ts-expect-error
+      (element) => element.name === "file"
+    );
+    const formData = new FormData();
+    // @ts-expect-error
+    for (const file of fileInput?.files) {
+      formData.append(inputName, file);
+    }
+    formData.append("upload_preset", "codery");
+    const endpoint = "https://api.cloudinary.com/v1_1/ntabucejo/image/upload";
+    const response = await fetch(endpoint, { method: "POST", body: formData });
+    const data = await response.json();
+
+    setData(data.secure_url);
+
+    return data;
+  };
+
+  const handleChange = (event: FormEvent<HTMLFormElement>) => {
+    const reader = new FileReader();
+    reader.onload = function (onLoadEvent) {
+      // @ts-expect-error
+      setData(onLoadEvent.target.result);
+    };
+    // @ts-expect-error
+    reader.readAsDataURL(event.target.files[0]);
+  };
+
+  return { handleSubmit, handleChange };
+};
+
+export default useUpload;
