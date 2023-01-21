a8c337b363c3cbeedd3042b27d8531a560300dff
jazztine <jzztn.crz@gmail.com>
2023-01-21 19:24:52 +0800

    chore(fileds): modify file input field

--- core/components/elements/fields/file.tsx
+++ core/components/elements/fields/file.tsx
@@ -12,7 +12,7 @@ const FileField = ({ handleChange, className }: Props) => {
       type="file"
       className={`${
         className ? className : ""
-      } rounded border-[1px] border-primary-dark/25 bg-transparent p-3 pl-4 outline-1 outline-primary-dark/25`}
+      }`}
     />
   );
 };
