884c619e7598f641c622491b9f9252e1bdd9bf17
jazztine <jzztn.crz@gmail.com>
2023-01-21 14:00:55 +0800

    chore(gig): modify create gig overview

--- core/components/sections/gig/create/overview.tsx
+++ core/components/sections/gig/create/overview.tsx
@@ -2,7 +2,9 @@ import Button from "@core/components/elements/button";
 import Categories from "@core/components/elements/fields/categories";
 import Description from "@core/components/elements/fields/description";
 import Label from "@core/components/elements/fields/label";
-import TextField from "@core/components/elements/fields/textField";
+import NumberField from "@core/components/elements/fields/number";
+import TextAreaField from "@core/components/elements/fields/textarea";
+import TextField from "@core/components/elements/fields/text";
 import Tooltip from "@core/components/elements/tooltip";
 import React from "react";
 
@@ -21,7 +23,7 @@ const Overview = () => {
             </Tooltip>
           </div>
           <Description paragraph="This will help your gig to recognize instantaneously." />
-          <TextField />
+          <TextField placeholder="I will create you ..." />
         </div>
 
         {/* project description */}
@@ -34,8 +36,10 @@ const Overview = () => {
               </p>
             </Tooltip>
           </div>
-          <Description paragraph="As your Gig storefront, your title is the most important place to include keywords that buyers would likely use to search for a service like yours." />
-          <TextField />
+          <Description paragraph="Briefly Describe Your Gig" />
+          <TextAreaField
+            placeholder="Description of your gig"
+          />
         </div>
 
         <div className="grid gap-6 laptop:grid-cols-2">
@@ -67,6 +71,20 @@ const Overview = () => {
             <Description paragraph="Tag your Gig with buzz words that are relevant to the services you offer. Use all 5 tags to get found." />
             <Categories />
           </div>
+        </div>
+
+        {/* project price */}
+        <div className="flex flex-col gap-1">
+          <div className="flex items-center gap-2">
+            <Label name="Starting Price" />
+            <Tooltip>
+              <p className="whitespace-nowrap">
+                All prices should start from 50 dollars.
+              </p>
+            </Tooltip>
+          </div>
+          <Description paragraph="How much is your starting price? You can negotiate with your client about the final amount later." />
+          <NumberField />
         </div>
       </div>
       <Button className="ml-auto mt-4">Save & Continue</Button>
