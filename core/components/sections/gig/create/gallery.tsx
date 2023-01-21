19dee03f20536fa22a2263743d4591121f377e86
jazztine <jzztn.crz@gmail.com>
2023-01-21 19:24:37 +0800

    chore(create-gig): modify create gig's gallery

--- core/components/sections/gig/create/gallery.tsx
+++ core/components/sections/gig/create/gallery.tsx
@@ -1,6 +1,8 @@
 "use client";
+import Button from "@core/components/elements/button";
 import FileField from "@core/components/elements/fields/file";
-import { PhotoIcon } from "@heroicons/react/24/solid";
+import Symbol from "@core/components/elements/symbol";
+import { PhotoIcon, TrashIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 import { useState } from "react";
 
@@ -32,10 +34,10 @@ const Gallery = () => {
             setMaximumError("You can add up to 8 images only.");
           }
         }}
-        className="grid w-full cursor-pointer place-items-center border-2 border-dashed p-6">
+        className="grid w-full cursor-pointer place-items-center border-2 border-dashed p-4">
         <div className="flex items-center gap-4">
-          <PhotoIcon className="h-6 w-6" />
-          <span>Click to add another image</span>
+          <PhotoIcon className="h-6 w-6 text-primary-dark/60" />
+          <span className="text-primary-dark/60">Click to add another image</span>
         </div>
       </button>
 
@@ -44,39 +46,27 @@ const Gallery = () => {
         <p className="text-sm font-light">
           Get noticed by the right buyers with visual examples of your services.
         </p>
-      </div>
-
-      <div className="grid grid-cols-2 gap-6 laptop:grid-cols-3">
-        {addImage.map((image, index) => (
-          <div
-            key={image.id}
-            className="relative grid cursor-pointer place-items-center gap-4 border-2 border-dashed p-6">
-            <div className="absolute -top-4 -left-3 z-50  border-2 bg-white p-1 px-3 font-semibold">
-              {image.id}
-            </div>
-            <div className="relative h-80 w-80 overflow-hidden">
-              <Image
-                src="https://images.unsplash.com/photo-1593341832681-6cb2ce1ed053?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjh8fHh8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
-                alt="Image"
-                fill
-                className="object-cover"
-              />
-            </div>
-            <FileField
-              handleChange={(event) => {
-                if (image.id === index) {
-                  setAddImage([
-                    ...addImage,
-                    { id: image.id, src: event.target.value },
-                  ]);
-                }
-              }}
-            />
-          </div>
-        ))}
         <span className="infinite mt-auto cursor-pointer text-xs text-red-500 transition duration-150 ease-in-out hover:animate-shake">
           {maximumError}
         </span>
+      </div>
+
+      <div className="grid grid-cols-2 items-center gap-4 tablet:grid-cols-3 laptop:grid-cols-5">
+        {addImage.map((image, index) => (
+          <div
+            key={image.id}
+            className="group flex h-40 w-full cursor-pointer flex-col items-center justify-center border-2 border-dashed tablet:w-60">
+            <Symbol
+              Icon={PhotoIcon}
+              size="large"
+              className="text-primary-dark/40"
+            />
+            <span className="text-sm text-primary-dark/40">
+              Drag & Drop a photo
+            </span>
+            <span className="text-sm text-primary-dark/40">Browse</span>
+          </div>
+        ))}
       </div>
     </div>
   );
