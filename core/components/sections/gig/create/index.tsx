224907c471d549efdf82e398bd54747037923d23
jazztine <jzztn.crz@gmail.com>
2023-01-21 14:27:56 +0800

    chore(gig): modify create gig overview

--- core/components/sections/gig/create/index.tsx
+++ core/components/sections/gig/create/index.tsx
@@ -1,25 +1,42 @@
 "use client";
 import { Tab } from "@headlessui/react";
-import React from "react";
+import React, { useState } from "react";
 import Overview from "./overview";
 
 const panels = [
-  { title: "Overview", content: <Overview /> },
-  { title: "Gallery", content: "Content 5" },
-  { title: "Publish", content: "Content 6" },
+  { id: 1, title: "Overview", content: <Overview /> },
+  { id: 2, title: "Gallery", content: "Content 5" },
+  { id: 3, title: "Publish", content: "Content 6" },
 ];
 
 const CreateGig = () => {
+  const [currentStep, setCurrentStep] = useState(1);
+
   return (
-    <div className="contain grid gap-8">
+    <div className="contain my-4 grid gap-8">
       <Tab.Group>
-        <Tab.List className="flex items-center overflow-scroll scrollbar-hide gap-4">
+        <Tab.List className="flex items-center gap-6 overflow-scroll scrollbar-hide">
           {panels.map((panel) => (
-            <Tab
-              key={panel.title}
-              className="cursor-pointer outline-none smooth hover:font-medium">
-              {panel.title}
-            </Tab>
+            <div key={panel.id} onClick={() => setCurrentStep(panel.id)}>
+              <div className="group flex cursor-pointer items-center gap-2">
+                <div
+                  className={`${
+                    panel.id === currentStep ? "bg-primary-dark text-white" : ""
+                  } rounded-full border-2 p-1 px-3 font-semibold`}>
+                  {panel.id}
+                </div>
+                <Tab
+                  key={panel.title}
+                  className="smooth cursor-pointer font-medium outline-none">
+                  {panel.title}
+                </Tab>
+                {panel.title !== "Publish" ? (
+                  <hr className="h-[1px] w-10 bg-gray-700 tablet:w-36" />
+                ) : (
+                  ""
+                )}
+              </div>
+            </div>
           ))}
         </Tab.List>
         <Tab.Panels>
