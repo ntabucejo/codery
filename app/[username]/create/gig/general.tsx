255f37eecb5f43c10f48e1147d0ae33bd8c5d28a
ntabucejo <ntabucejo@gmail.com>
2023-01-22 19:25:24 +0800

    chore(select): add multiple select

--- app/[username]/create/gig/general.tsx
+++ app/[username]/create/gig/general.tsx
@@ -49,6 +49,13 @@ const General = () => {
           <Field.Select.List options={options} />
         </Field.Body>
       </div>
+      <Field.Body
+        id="Technologies"
+        label="Technologies"
+        description="How much is your starting price? You can negotiate with your client about the final amount later."
+        tooltip="All prices should start from 50 dollars.">
+        <Field.Select.Multiple options={options} />
+      </Field.Body>
     </form>
   );
 };
---
+++ core/components/elements/field/select/multiple.tsx
@@ -0,0 +1,110 @@
+"use client";
+
+import { Fragment, useState } from "react";
+import { Combobox, Transition } from "@headlessui/react";
+import { ChevronUpDownIcon } from "@heroicons/react/20/solid";
+import Symbol from "../../symbol";
+
+type Option = {
+  id: number;
+  name: string;
+};
+
+type Props = {
+  options: Option[];
+};
+
+const Multiple = ({ options }: Props) => {
+  const [selected, setSelected] = useState<Option>({ id: -1, name: "Select" });
+  const [query, setQuery] = useState("");
+  const [value, setValue] = useState("Select");
+  const [items, setItems] = useState<Option[]>([]);
+
+  const filteredOptions =
+    query === ""
+      ? options
+      : options.filter((option) =>
+          option.name
+            .toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.toLowerCase().replace(/\s+/g, ""))
+        );
+
+  const isItemExists = (option: Option) => {
+    if (items.find((item) => item.id === option.id)) return true;
+    return false;
+  };
+
+  const handleAddItem = (option: Option) => {
+    if (!isItemExists(option)) {
+      setItems([...items, option]);
+    }
+  };
+
+  return (
+    <div className="space-y-2">
+      <Combobox value={selected} onChange={setSelected}>
+        <div className="relative text-sm">
+          <Combobox.Input
+            onBlur={() => setValue("Select")}
+            className="clearance w-full rounded border"
+            onClick={() => setValue("")}
+            displayValue={() => value}
+            onChange={(event) => setQuery(event.target.value)}
+          />
+          <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+            <Symbol Icon={ChevronUpDownIcon} size="small" />
+          </Combobox.Button>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => setQuery("")}>
+            <Combobox.Options className="absolute z-10 mt-2 max-h-60 w-full overflow-auto rounded bg-white shadow">
+              {filteredOptions.length === 0 && query !== "" ? (
+                <div className="clearance relative cursor-default select-none text-gray-700">
+                  Nothing found.
+                </div>
+              ) : (
+                filteredOptions.map((option) => (
+                  <Combobox.Option
+                    key={option.id}
+                    value={option}
+                    onClick={() => handleAddItem(option)}
+                    className={({ active }) =>
+                      `${
+                        active
+                          ? "bg-primary-dark text-primary-light"
+                          : "text-primary-dark"
+                      } clearance relative cursor-default select-none`
+                    }>
+                    {({ selected }) => (
+                      <span
+                        className={`block truncate ${
+                          selected || isItemExists(option) ? "line-through" : ""
+                        }`}>
+                        {option.name}
+                      </span>
+                    )}
+                  </Combobox.Option>
+                ))
+              )}
+            </Combobox.Options>
+          </Transition>
+        </div>
+      </Combobox>
+      <ul className="flex gap-2">
+        {items.map((item) => (
+          <li
+            key={item.id}
+            className="rounded bg-primary-dark/fade px-2 py-1 text-sm text-primary-light">
+            {item.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Multiple;
