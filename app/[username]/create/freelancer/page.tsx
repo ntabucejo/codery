995d86303065606132b222a2986c7563b8a627e6
jazztine <jzztn.crz@gmail.com>
2023-01-22 17:52:48 +0800

    fix(freelancer): rename page to about and create new page

--- app/[username]/create/freelancer/page.tsx
+++ app/[username]/create/freelancer/page.tsx
@@ -1,29 +1,10 @@
-import Stages from "@core/components/elements/stages";
-import Education from "./education";
-import PersonalInformation from "./personal-information";
+import About from "./about";
 
 const Page = () => {
-  const panels = [
-    { id: 1, title: "Personal Information", content: <PersonalInformation /> },
-    {
-      id: 2,
-      title: "Skills and Technologies",
-      content: <PersonalInformation />,
-    },
-    {
-      id: 3,
-      title: "Education",
-      content: <Education />,
-    },
-  ];
-
   return (
-    <>
-      <section className="contain space-y-4">
-        <h1 className="text-4xl font-bold">Become a Freelancer</h1>
-        <Stages panels={panels} />
-      </section>
-    </>
+    <div>
+      <About />
+    </div>
   );
 };
 
