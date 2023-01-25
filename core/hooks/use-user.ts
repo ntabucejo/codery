b08828d45473c6468f74b38a024e7fd88c578eb9
ntabucejo <ntabucejo@gmail.com>
2023-01-25 20:57:29 +0800

    chore(profile): update profile

--- core/hooks/use-user.ts
+++ core/hooks/use-user.ts
@@ -1,11 +1,25 @@
 import prisma from "@core/libraries/prisma";
 import useSession from "./use-session";
 
-const useUser = async () => {
+const useUser = async (username: string) => {
   const session = await useSession();
-  const user = await prisma.user.findUnique({
-    where: { email: session?.user?.email! || "!" },
-  });
+
+  const getUser = async () => {
+    if (username) {
+      const user = await prisma.user.findUnique({
+        where: {
+          username: username,
+        },
+      });
+      return user;
+    }
+    const user = await prisma.user.findUnique({
+      where: { email: session?.user?.email! || "!" },
+    });
+    return user;
+  };
+
+  const user = await getUser();
 
   return user;
 };
