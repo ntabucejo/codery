1c6999527c42dbf41dc44460e6355b6e3fd2ca9b
jazztine <jzztn.crz@gmail.com>
2023-01-25 20:33:24 +0800

    chore(validations): add error messages

--- core/validations/schemas/gig/index.ts
+++ core/validations/schemas/gig/index.ts
@@ -4,19 +4,29 @@ import thumbnail from "./thumbnail";
 const gig = z.object({
   title: z
     .string()
-    .startsWith("I will", "Project title must start with 'I will'")
-    .max(80),
-  description: z.string().min(10).max(1000),
-  category: z.object({ id: z.string().cuid(), name: z.string() }),
+    .startsWith("I will", "Gig title must start with 'I will'.")
+    .max(80, "Gig Title must not exceed 80 characters."),
+  description: z
+    .string()
+    .min(10, "Description must have at least 10 characters.")
+    .max(1000, "Description must not exceed to 1000 characters."),
+  category: z.object({ id: z.string().cuid("Gig should have a category."), name: z.string() }),
   tags: z
     .object({ id: z.string().cuid(), name: z.string() })
     .array()
-    .min(1)
-    .max(12),
-  thumbnails: thumbnail.array().min(1).max(8),
-  from: z.number().positive().min(5),
-  to: z.number().positive().max(10000),
-  period: z.number().positive(),
+    .min(1, "Each Gig should have at least 1 tag.")
+    .max(12, "Gig should not have more than 12 tags."),
+  thumbnails: thumbnail
+    .array()
+    .min(1, "Each Gig should have at least 1 thumbnail.")
+    .max(8, "Each Gig should not have more than 8 thumbnails.")
+    .optional(),
+  from: z.number().positive("Price should be positive only.").min(5, "Price should starts with 5 dollars."),
+  to: z
+    .number()
+    .positive("Price should be positive only.")
+    .max(10000, "Price should not exceed more than 10000 dollars."),
+  period: z.number().positive("Period should be positive only."),
   isPublished: z.boolean(),
 });
 
