d9226a3457cd0f7c08f0411cc3b537452f038bf2
Jazztine Cruz <jzztn.crz@gmail.com>
2023-05-21 18:34:00 +0800

    chore(api): add get gig

---
+++ pages/api/gigs/[gigId]/index.tsx
@@ -0,0 +1,23 @@
+import prisma from "@core/libraries/prisma";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const handler = async (request: NextApiRequest, response: NextApiResponse) => {
+  if (request.method !== "GET")
+    return response.json({ message: "Invalid Method" });
+
+  try {
+    const gigId = request.query.gigId;
+    const gig = await prisma.gig.findUnique({
+      where: {
+        id: String(gigId),
+      },
+    });
+
+    return response.json(gig);
+  } catch (error) {
+    console.error({ error });
+    return response.json({ message: "Internal Server Error" });
+  }
+};
+
+export default handler;
