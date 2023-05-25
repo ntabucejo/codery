729aa7f1e6680b8820dafe26f2e38ac231d10a35
Jazztine Cruz <jzztn.crz@gmail.com>
2023-05-25 13:06:58 +0800

    working on chat

---
+++ pages/api/data/users/[userId]/reviews/index.ts
@@ -0,0 +1,25 @@
+import prisma from "@core/libraries/prisma";
+import { type NextApiRequest, type NextApiResponse } from "next";
+
+const handler = async (request: NextApiRequest, response: NextApiResponse) => {
+  if (request.method === "POST") {
+    const { userId } = request.query;
+    const { body } = request;
+    try {
+      const review = await prisma.review.create({
+        data: {
+          message: body.message,
+          rating: body.rating,
+          gigId: body.gigId,
+          clientId: String(userId),
+        },
+      });
+      response.status(201).send({ message: "OK", review });
+    } catch (error) {
+      console.error(error);
+      response.status(500).send({ error });
+    }
+  }
+};
+
+export default handler;
