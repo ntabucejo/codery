70a964d8bab91cd35a82a407651d12311a6a5494
nikkoabucejo <ntabucejo@gmail.com>
2023-05-27 20:25:57 +0800

    chore add chat fetch

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import useUser from "@core/hooks/use-user";
+import useSession from "../../core/hooks/use-session";
+import prisma from "@core/libraries/prisma";
+import ChatView from "./chat-view";
 
-const Chat = () => {
+const Chat = async () => {
+  const user = await useUser();
+
+  const asClientMessages = await prisma.message.findMany({
+    where: {
+      userId: user?.id,
+    },
+    include: {
+      freelancer: {
+        include: {
+          user: true,
+        },
+      },
+    },
+  });
+
+  const asFreelancerMessages = await prisma.message.findMany({
+    where: {
+      freelancerId: user?.freelancer?.id,
+    },
+    include: {
+      user: true,
+    },
+  });
+
+  console.log({ asClientMessages, asFreelancerMessages, user });
+
   return (
-    <div>Chat</div>
-  )
-}
+    <div>
+      <ChatView
+        asClientMessages={asClientMessages}
+        asFreelancerMessages={asFreelancerMessages}
+      />
+    </div>
+  );
+};
 
-export default Chat
+export default Chat;
--- core/hooks/use-user.ts
+++ core/hooks/use-user.ts
@@ -6,15 +6,15 @@ const useUser = async (query?: Prisma.UserFindUniqueArgs) => {
   const session = await useSession();
 
   const getUser = async () => {
-    if (query) {
-      const user = await prisma.user.findUnique(query);
-      return user;
-    }
+    // if (query) {
+    //   const user = await prisma.user.findUnique(query);
+    //   return user;
+    // }
     const user = await prisma.user.findUnique({
       where: { email: session?.user?.email! || "!" },
       include: {
-        freelancer: true
-      }
+        freelancer: true,
+      },
     });
     return user;
   };
