431ac94fb87452bd7cf240ba1177115ffec8bcd4
Jazztine Cruz <jzztn.crz@gmail.com>
2023-06-04 18:04:29 +0800

    Add rules and regulations for payment

--- core/components/modals/offer/recieved-offer-details.tsx
+++ core/components/modals/offer/recieved-offer-details.tsx
@@ -19,9 +19,10 @@ type Props = {
   };
 };
 const RecievedOfferDetailsModal = ({ offer, modal, user }: Props) => {
-  if(!offer) return <></>
-  
+  if (!offer) return <></>;
+
   const paymentModal = useModal();
+  const paymentRulesModal = useModal();
 
   return (
     <>
@@ -69,7 +70,9 @@ const RecievedOfferDetailsModal = ({ offer, modal, user }: Props) => {
         </div>
 
         <div className="flex w-full gap-4">
-          <Button onClick={paymentModal.handleOpen}>Accept Offer</Button>
+          <Button onClick={paymentRulesModal.handleOpen}>
+            Accept Offer and Pay
+          </Button>
           <Button variant="secondary" onClick={modal.handleClose}>
             Decline Offer
           </Button>
@@ -83,6 +86,106 @@ const RecievedOfferDetailsModal = ({ offer, modal, user }: Props) => {
       </Modal>
 
       <PaymentModal user={user} modal={paymentModal} offferId={offer.id} />
+
+      <Modal
+        title="Rules and Regulations: Payment"
+        description="By Clicking Continue, this will prove that you agreed to our rules and regulations before paying."
+        state={paymentRulesModal.state}
+        handleClose={paymentRulesModal.handleClose}
+        className="z-[999] max-w-5xl">
+        <div className="flex flex-col items-center gap-5">
+          <div className="h-96 overflow-y-scroll">
+            <p>
+              Codery Rules and Regulations of Payment
+              <br />
+              <br />
+              <span className="font-semibold">1. Payment Process</span>
+              <br />
+              1.1 All payments between clients and freelancers on Codery must be
+              processed through the platform's designated payment system.
+              <br />
+              1.2 Clients are required to provide valid payment information and
+              authorize payments for the services rendered by the freelancer.
+              <br />
+              1.3 Freelancers must ensure that the payment details provided are
+              accurate and up to date.
+              <br />
+              <br />
+              <span className="font-semibold">2. Service Fees</span>
+              <br />
+              2.1 Codery charges a service fee for each successful transaction
+              conducted on the platform. The fee structure will be clearly
+              outlined and communicated to freelancers and clients.
+              <br />
+              2.2 The service fee will be deducted automatically from the total
+              payment made by the client to the freelancer.
+              <br />
+              <br />
+              <span className="font-semibold">3. Payment Disputes</span>
+              <br />
+              3.1 In the event of a payment dispute, both the client and the
+              freelancer should attempt to resolve the matter amicably and
+              professionally.
+              <br />
+              3.2 If a resolution cannot be reached, either party may raise a
+              dispute with Codery, providing relevant evidence and documentation
+              to support their claim.
+              <br />
+              3.3 Codery will review the dispute and make a fair and impartial
+              decision based on the available information.
+              <br />
+              3.4 The decision made by Codery in payment dispute cases will be
+              final and binding.
+              <br />
+              <br />
+              <span className="font-semibold">
+                4. Payment Security and Confidentiality
+              </span>
+              <br />
+              4.1 Codery takes payment security seriously and employs
+              industry-standard measures to protect payment information.
+              <br />
+              4.2 Freelancers and clients should also take precautions to ensure
+              the security and confidentiality of their payment-related
+              communications and information.
+              <br />
+              <br />
+              <span className="font-semibold">
+                5. Refunds and Cancellations
+              </span>
+              <br />
+              5.1 Refunds and cancellations are subject to the specific terms
+              and conditions agreed upon between the client and the freelancer.
+              <br />
+              5.2 Freelancers should clearly communicate their refund policy,
+              including any conditions or fees associated with cancellations or
+              partial refunds.
+              <br />
+              5.3 Codery is not responsible for issuing refunds or mediating
+              disputes related to refunds, as this is primarily the
+              responsibility of the freelancer and the client.
+              <br />
+              <br />
+              <span className="font-semibold">6. Tax Obligations</span>
+              <br />
+              6.1 Freelancers are solely responsible for complying with their
+              tax obligations related to the income received through Codery.
+              <br />
+              6.2 Freelancers should consult with their tax advisors to
+              understand and fulfill their tax obligations in their respective
+              jurisdictions.
+              <br />
+            </p>
+          </div>
+          <Button
+            onClick={() => {
+              paymentRulesModal.handleClose();
+              paymentModal.handleOpen();
+            }}>
+            I agree and Continue
+          </Button>
+        </div>
+      </Modal>
     </>
   );
 };
