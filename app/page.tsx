deca5cbd584ad9e97a4872259b5d823a1e850b47
Jazztine Cruz <jzztn.crz@gmail.com>
2023-06-05 12:00:12 +0800

    fix dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import Hero from "@core/components/sections/hero";
 import prisma from "@core/libraries/prisma";
+import useSession from "@core/hooks/use-session";
+
 import FilteredGigs from "./gigs";
 
 const Page = async () => {
+  const session = await useSession();
   const categories = await prisma.category.findMany();
   const gigs = await prisma.gig.findMany({
     include: {
@@ -20,7 +23,9 @@ const Page = async () => {
   return (
     <>
       <Hero image="https://images.unsplash.com/photo-1595776613215-fe04b78de7d0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" />
-      <FilteredGigs categories={categories} gigs={gigs} />
+      {session?.user ? (
+        <FilteredGigs categories={categories} gigs={gigs} />
+      ) : null}
     </>
   );
 };
